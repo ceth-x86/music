4c06e0e5a5e3b06bb7202fde1400c5777f5f4b49
Andery Demidov <andrey.demidov@gmail.com>
2019-09-01 19:17:26 +0300

    Change command structure

--- cmd/playlist.go
+++ cmd/playlist.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"os"
-
 	settings2 "github.com/demas/music/internal/services/settings"
 
 	"github.com/demas/music/internal/models/core"
@@ -17,10 +15,16 @@ var Service string
 var Id string
 
 // addPlaylistCmd represents the addPlaylist command
-var addPlaylistCmd = &cobra.Command{
+var playistCmd = &cobra.Command{
 	Use:   "playlist",
 	Short: "Working with playlists",
-	Long: `Working with playlists. Examples:
+	Args:  cobra.MinimumNArgs(1),
+}
+
+var addPlaylistCommand = &cobra.Command{
+	Use:   "add",
+	Short: "Adding playlists",
+	Long: `Adding playlists. Examples:
 		playlist add --service spoify --id dAKJ23K4H234
 		playlist add --service deezer --id 23424
 		playlist add --service yandex --id 3123123123
@@ -32,39 +36,33 @@ var addPlaylistCmd = &cobra.Command{
 			_ = logger.Sync()
 		}()
 
-		if len(args) == 0 {
-			logger.Errorw("command is missing")
-			os.Exit(0)
+		logger.Infow("adding playlist",
+			"service", Service,
+			"id", Id)
+
+		settings := settings2.InitSettings()
+		db, err := dbutils.OpenDbConnection(settings.DbConnectionString, settings.TraceSqlCommand)
+		if err != nil {
+			logger.With(zap.Error(err)).Error("не удалось установить соединение с PostgreSQL")
 		}
 
-		switch args[0] {
-		case "add":
-			logger.Infow("adding playlist",
-				"service", Service,
-				"id", Id)
-
-			settings := settings2.InitSettings()
-			db, err := dbutils.OpenDbConnection(settings.DbConnectionString, settings.TraceSqlCommand)
-			if err != nil {
-				logger.With(zap.Error(err)).Error("не удалось установить соединение с PostgreSQL")
-			}
-
-			repository := datastore.NewPlaylistRepository(db)
-			_, err = repository.Store(&core.Playlist{
-				Service:    Service,
-				PlaylistId: Id,
-			})
-			if err != nil {
-				logger.With(zap.Error(err)).Error("не удалось создать плейлист")
-			}
-		default:
-			logger.Errorw("unknown command")
+		repository := datastore.NewPlaylistRepository(db)
+		_, err = repository.Store(&core.Playlist{
+			Service:    Service,
+			PlaylistId: Id,
+		})
+		if err != nil {
+			logger.With(zap.Error(err)).Error("не удалось создать плейлист")
 		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(addPlaylistCmd)
-	addPlaylistCmd.Flags().StringVarP(&Service, "service", "s", "", "Music service")
-	addPlaylistCmd.Flags().StringVarP(&Id, "id", "i", "", "Playlist id")
+	rootCmd.AddCommand(playistCmd)
+
+	playistCmd.AddCommand(addPlaylistCommand)
+	addPlaylistCommand.Flags().StringVarP(&Service, "service", "s", "", "Music service")
+	addPlaylistCommand.Flags().StringVarP(&Id, "id", "i", "", "Playlist id")
+	_ = addPlaylistCommand.MarkFlagRequired("service")
+	_ = addPlaylistCommand.MarkFlagRequired("id")
 }
