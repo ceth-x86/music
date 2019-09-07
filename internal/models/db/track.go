ca42a45ca1fc37279e2c002ce685f9f1c0e2c216
Andery Demidov <andrey.demidov@gmail.com>
2019-09-07 17:43:10 +0300

    Unique constraint for Tracks

--- internal/models/db/track.go
+++ internal/models/db/track.go
@@ -4,9 +4,9 @@ import "github.com/jinzhu/gorm"
 
 type Track struct {
 	gorm.Model
-	PlaylistId uint `gorm:"TYPE:integer REFERENCES playlists"`
+	PlaylistId uint `gorm:"TYPE:integer REFERENCES playlists;unique_index:idx_playlist_track_id"`
 	Name       string
-	TrackId    string
+	TrackId    string `gotm:"unique_index:idx_playlist_track_id"`
 	// Artist           string
 	// Album            string
 }
