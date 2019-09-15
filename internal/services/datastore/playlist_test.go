6dcfb614b91ae6336da4a47587e17c01483f5416
Andery Demidov <andrey.demidov@gmail.com>
2019-09-15 18:23:22 +0300

    Unit test: PlaylistRepository.Fetch() + Refactore

--- internal/services/datastore/playlist_test.go
+++ internal/services/datastore/playlist_test.go
@@ -1,13 +1,58 @@
 package datastore
 
 import (
+	"database/sql"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/demas/music/internal/models/core"
+
+	"github.com/stretchr/testify/suite"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jinzhu/gorm"
 )
 
-func TestGetPlaylistById(t *testing.T) {
+type Suite struct {
+	suite.Suite
+	DB   *gorm.DB
+	mock sqlmock.Sqlmock
+
+	repository *PlaylistRepository
+	playlist   *core.Playlist
+}
+
+func (suite *Suite) SetupTest() {
+
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	db, suite.mock, err = sqlmock.New()
+	require.NoError(suite.T(), err)
+
+	suite.DB, err = gorm.Open("postgres", db)
+	require.NoError(suite.T(), err)
+
+	suite.DB.LogMode(true)
+	suite.repository = NewPlaylistRepository(suite.DB)
+}
+
+func (suite *Suite) AfterTest(_, _ string) {
+	require.NoError(suite.T(), suite.mock.ExpectationsWereMet())
+}
+
+func (suite *Suite) TestFetch() {
+
+	suite.mock.ExpectQuery(`^SELECT \* FROM "playlists" WHERE \(deleted_at is null\)`).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "service", "playlist_id"}))
+
+	suite.repository.Fetch()
+}
+
+func (suite *Suite) TestGetPlaylistById() {
 
 	var (
 		id          = 1
@@ -15,22 +60,14 @@ func TestGetPlaylistById(t *testing.T) {
 		playlist_id = "123"
 	)
 
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	mock.ExpectQuery(`^SELECT \* FROM "playlists*"`).
+	suite.mock.ExpectQuery(`^SELECT \* FROM "playlists" WHERE ("playlist"."id" = ?)*`).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "service", "playlist_id"}).
 			AddRow(id, service, playlist_id))
 
-	gormDB, _ := gorm.Open("postgres", db)
-	repository := NewPlaylistRepository(gormDB.LogMode(true))
-	_, err = repository.GetById(uint(id))
+	_, err := suite.repository.GetById(uint(id))
+	require.NoError(suite.T(), err)
+}
 
-	// we make sure that all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
+func TestExampleTestSuite(t *testing.T) {
+	suite.Run(t, new(Suite))
 }
