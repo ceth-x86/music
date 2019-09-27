0dd718eb85da61b2d63da03658447a73862a4287
Andery Demidov <andrey.demidov@gmail.com>
2019-09-27 21:26:25 +0300

    Unique constraint: album.{Artist_id+Name}

--- internal/models/db/album.go
+++ internal/models/db/album.go
@@ -9,8 +9,8 @@ import (
 type Album struct {
 	gorm.Model
 	AlbumId     string `gorm:"unique_index"`
-	Name        string
-	ArtistId    uint `gorm:"TYPE:integer REFERENCES artists"`
+	Name        string `gorm:"unique_index:idx_artist_id_name"`
+	ArtistId    uint   `gorm:"TYPE:integer REFERENCES artists;unique_index:idx_artist_id_name"`
 	Popularity  int
 	AlbumType   string
 	ReleaseDate time.Time
